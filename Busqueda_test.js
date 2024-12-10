4b6b8cf17002579688bfc7403fe9aa5efa526316
Christian Humberto Gonzalez <gonzalezchh@globalhitss.com>
2024-12-10 08:40:39 -0800

    commit inicial

--- Busqueda_test.js
+++ Busqueda_test.js
@@ -12,5 +12,12 @@ Scenario('make a search by folioPisa',  ({ I }) => {
     I.click('#busqueda_general_button');
     I.fillField('#folio_pisa', '36016477');
     I.click('#busqueda_dropdown > div > div.px-3 > div.d-flex.justify-content-end > button');
+    I.usePlaywrightTo('handle new window', async ({ browserContext, page }) => {
+        page.on('popup', async (newPage) => {
+            const content = await newPage.textContent('body > div:nth-child(2) > div:nth-child(1) > table > tbody > tr:nth-child(2) > td:nth-child(2) > input[type=text]');
+            console.log(content);
+            expect(content).to.include('36016477', 'La información de la ventana emergente no corresponde al folioPisa buscado');
+        });
+      });
     I.saveScreenshot('busquedaFolioPisa.png');
 });
--- step_definitions/hacerBusquedaGeneral.js
+++ step_definitions/hacerBusquedaGeneral.js
@@ -10,7 +10,6 @@ Given('I opened the general search tab', () => {
 
 When('I enter the folio "36016477"', () => {
   I.fillField('#folio_pisa', '36016477');
-  I.click('#busqueda_dropdown > div > div.px-3 > div.d-flex.justify-content-end > button');
 });
 
 When('I click on the button "Buscar"', () => {
@@ -18,5 +17,12 @@ When('I click on the button "Buscar"', () => {
 });
 
 Then('A pop up should appear with the job information for the folioPisa "36016477"', () => {
+  I.usePlaywrightTo('handle new window', async ({ browserContext, page }) => {
+    page.on('popup', async (newPage) => {
+      const content = await newPage.textContent('body > div:nth-child(2) > div:nth-child(1) > table > tbody > tr:nth-child(2) > td:nth-child(2) > input[type=text]');
+      console.log(content);
+      expect(content).to.include('36016477', 'La información de la ventana emergente no corresponde al folioPisa buscado');
+    });
+  });
   I.saveScreenshot('busquedaFolioPisa.png');
 });
